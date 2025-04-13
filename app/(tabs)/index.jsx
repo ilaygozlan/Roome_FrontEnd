1e8c2a81e47571bb6664ad5a7bada9f359a69575
OfriG <ofri900@gmail.com>
2025-04-13 16:07:44 +0300

    .

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,26 +1,20 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet, ScrollView, View, Button } from "react-native";
-import Apartment from "../components/apartment";
-import { useRouter } from "expo-router";
-import LogoutButton from "../components/LogoutButton";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
-import UserProgressTrack from "../components/userProgressTrack";
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, ScrollView, View ,Button} from 'react-native'
-import Apartment from '../components/apartment'
-import { useRouter } from "expo-router";
 import { useState, useEffect } from 'react';
+import { useRouter } from "expo-router";
 import { registerForPushNotificationsAsync } from '../components/pushNatification';
-import API from "../../config";
-import LogoutButton from "../components/LogoutButton";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
+import Apartment from "../components/apartment";
+import LogoutButton from "../components/LogoutButton";
 import UserProgressTrack from "../components/userProgressTrack";
+import API from "../../config";
+
 
 export default function HomeScreen() {
   const router = useRouter();
   const [expoPushToken, setExpoPushToken] = useState('');
+  const [user, loading] = useAuthState(auth);
 
   // 🔄 Send push token to the server (as a plain string, not object!)
   const updatePushTokenOnServer = async (token) => {
