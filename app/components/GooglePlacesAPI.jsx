f64f61c6a46a8bf68adf47770ab6dd046fb744b9
AdiDagan1 <adidagan2271@gmail.com>
2025-07-06 12:35:31 +0300

    E

--- app/components/GooglePlacesAPI.jsx
+++ app/components/GooglePlacesAPI.jsx
@@ -1,7 +1,7 @@
+import 'react-native-get-random-values';
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { View, StyleSheet } from 'react-native';
-import { v4 as uuidv4 } from 'uuid';
 
 /**
  * @component GooglePlacesInput
