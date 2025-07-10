c66cfa6390e4afb43f9c93f4c8071dd47910d5a3
ilaygozlan <ilaygozlan@icloud.com>
2025-07-10 15:12:52 +0300

    pushi sushi

---
+++ setAdmin.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+// Initialize Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+async function setAdmin(uid) {
+  await admin.auth().setCustomUserClaims(uid, { admin: true });
+  console.log(`✅ User ${uid} is now an admin!`);
+}
+
+// Replace with your user's UID
+setAdmin("xhnFAsR5o4SmWjYczjiKSSXcgxv2");
